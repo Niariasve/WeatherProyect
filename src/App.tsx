e674ee712a29fb97817d3679d3ea4bfedbd00371
Niariasve <nestorariasve@gmail.com>
2024-06-17 20:33:56 -0500

    Guia 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Indicator from './components/indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <Grid>
-	      <Grid>1</Grid>
-	      <Grid>2</Grid>
-	      <Grid>3</Grid>
-	      <Grid>4</Grid>
-	      <Grid>5</Grid>
-	      <Grid>6</Grid>
+		<Grid container spacing={5}>
+			<Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
+	      	<Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
+	      	<Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
+	      	<Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
+			<Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
+			<Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+			<Grid xs={12} sm={6} md={4} lg={4}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
+			</Grid>
+			<Grid xs={12} sm={6} md={4} lg={4}>
+				<Summary></Summary>
+			</Grid>
+			<Grid lgOffset={2} xs={12} ms={8} md={8} lg={8}>
+				<BasicTable></BasicTable>
+			</Grid>
 	    </Grid>
     </>
   )
