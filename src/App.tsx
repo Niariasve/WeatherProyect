8e59702145b096860291a2411e77bc38d6f7daa4
Niariasve <138833714+Niariasve@users.noreply.github.com>
2024-06-19 14:18:22 -0500

    aja

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,31 @@ function App() {
   return (
     <>
 		<Grid container spacing={5}>
-			<Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-	      	<Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-	      	<Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-	      	<Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-			<Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-			<Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
-			<Grid xs={12} sm={6} md={4} lg={4}>
+			<Grid xs={12} sm={6} md={4} lg={2}>
 				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
 			</Grid>
-			<Grid xs={12} sm={6} md={4} lg={4}>
+			<Grid xs={12} sm={6} md={4} lg={2}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
+			</Grid>
+			<Grid xs={12} sm={6} md={4} lg={2}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
+			</Grid>
+			<Grid xs={12} sm={6} md={4} lg={2}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
+			</Grid>
+			<Grid xs={12} sm={6} md={4} lg={2}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
+			</Grid>
+			<Grid xs={12} sm={6} md={4} lg={2}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
+			</Grid>
+			<Grid xs={12} sm={6} md={4} lg={2}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}/>
+			</Grid>
+			<Grid xs={12} sm={6} md={4} lg={2}>
 				<Summary></Summary>
 			</Grid>
-			<Grid xs={12} sm={8} md={8} lg={8} lgOffset={2}>
+			<Grid xs={12} sm={8} md={8} lg={10}>
 				<BasicTable></BasicTable>
 			</Grid>
 	    </Grid>
